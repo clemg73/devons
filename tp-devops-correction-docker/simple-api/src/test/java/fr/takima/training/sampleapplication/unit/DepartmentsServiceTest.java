b8ca49fde35661627dcafc5b2c90839cca339c3b
clem_grs <clemg.gros@gmail.com>
2025-02-04 16:25:42 +0100

    change pom

--- tp-devops-correction-docker/simple-api/src/test/java/fr/takima/training/sampleapplication/unit/DepartmentsServiceTest.java
+++ tp-devops-correction-docker/simple-api/src/test/java/fr/takima/training/sampleapplication/unit/DepartmentsServiceTest.java
@@ -29,7 +29,7 @@ class DepartmentsServiceTest {
 
     @Test
     void testGetDepartmentByName() {
-        when(departmentDAO.findDepartmentByName("DepartmentTest")).thenReturn(null);
+        when(departmentDAO.findDepartmentByName("DepartmentTest")).thenReturn(DEPARTMENT);
         assertEquals(DEPARTMENT, departmentDAO.findDepartmentByName("DepartmentTest"));
     }
 
